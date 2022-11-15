706adcfdc7907fee15e11067152986089ede79a3
GolovinD <golovin.dmytro@gmail.com>
2022-11-15 13:38:30 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/modal';
+import './js/menu-mobile';
+import './js/scroll';
+import './js/animation-on-scroll';
+import './js/hero-animation';
+import './js/products';
+import './js/slick-reviews';
